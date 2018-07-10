e06040c0c6c6afe5dbcc21b047fb1e5301857e69
Nathan West <nathanwest@google.com>
2018-07-09 21:42:32 -0700

    Some stuff

--- src/javascript/campaign-url-builder/components/campaign-url.js
+++ src/javascript/campaign-url-builder/components/campaign-url.js
@@ -320,18 +320,20 @@ export default class CampaignUrl extends React.Component {
     ) : null;
   }
 
-  renderBitlyNotice() {
-    return this.state.showBitlyNotice ?
-      <div className="CampaignUrlResult-bitlyNotice">
-        <span><Icon type="warning" /> <strong>Notice:</strong> Due to
+    renderBitlyNotice() {
+      // TODO(nathanwest) it'd be my preference to include an Icon here
+      // but I couldn't figure out how to get the css to work :(
+      return this.state.showBitlyNotice ?
+      <span className="CampaignUrlResult-bitlyNotice">
+        <strong>Notice:</strong> Due to
         the <a href="https://developers.googleblog.com/2018/03/transitioning-google-url-shortener.html">
         deprecation</a> of <a href="https://goo.gl/">Google's url shortener</a>,
         we will be transitioning the URL shortener on this page
         to <a href="https://bitly.com/">Bitly</a> on Monday, June 23, 2018, at
         or around 11am PST. The first time you use the new URL shortener, you
         will be prompted to authenticate with the Bitly API, so that we can
-        create shortlinks on your behalf.</span>
-      </div>
+        create shortlinks on your behalf.
+      </span>
     : null
   }
 
